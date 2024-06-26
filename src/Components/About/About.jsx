dcb7e2c4fa635803a37ddaee1002cdfb38a707e8
Michael Chukwu <imchukwu@gmail.com>
2024-06-26 08:25:56 +0100

    image deletion

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import about_img from "../../assets/about.png";
+import about_img from "../../assets/about.webp";
 import play_icon from "../../assets/play-icon.png";
 
 const About = ({ setPlayState }) => {
